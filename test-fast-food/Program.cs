14296f91a8a10a3ec2abb6ec993f94f12d98701d
szunini <seba.zunini@gmail.com>
2024-10-05 20:11:48 -0300

    initial commit

---
+++ test-fast-food/Program.cs
@@ -0,0 +1,15 @@
+namespace test_fast_food
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
